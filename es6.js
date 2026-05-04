ad0c9e087afe7e87ad76e11148abca975580800a
João Henrique Brasil <brasiljoaohenrique@gmail.com>
2026-05-04 19:06:41 -0300

    aula es6

---
+++ es6.js
@@ -0,0 +1,9 @@
+const nome = "João";
+
+let idade = 18;
+
+const apresentar = (nome, idade) => {
+    return `Olá, eu sou o ${nome}, e tenho ${idade} anos.`
+}
+
+console.log(apresentar(nome, idade));
